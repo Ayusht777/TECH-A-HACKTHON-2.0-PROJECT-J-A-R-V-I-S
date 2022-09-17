069bb92e1f1d13e0d16dab453a7e74156d41d192
Ayusht777 <talesarayush@gmail.com>
2022-09-17 22:15:12 +0530

    added functions

--- demo.py
+++ demo.py
@@ -2,9 +2,14 @@ import pyttsx3 # for text to speech
 from datetime import datetime # for time
 import speech_recognition as user_audio # user input by voice
 import pyaudio
+import os
+import subprocess as sp
 
-
-
+import requests
+import wikipedia
+import pywhatkit as kit
+import webbrowser 
+import smtplib
 
 
 USERNAME = 'AYUSH'
@@ -17,7 +22,7 @@ engine = pyttsx3.init()
 """ RATE"""
 rate = engine.getProperty('rate')   # getting details of current speaking rate
 # print (rate)                        #printing current voice rate
-engine.setProperty('rate', 110)     # setting up new voice rate
+engine.setProperty('rate', 121)     # setting up new voice rate
 
 
 """VOICE"""
@@ -57,7 +62,15 @@ def greet_user():
     
 
 # greet_user()
-
+def sendEmail(to, content):
+    smtplib = any
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.ehlo()
+    server.starttls()
+    server.login('divyanshusahucoc@gmail.com', 'your-password')
+    server.sendmail('divyashusahu@gmail.com', to, content)
+    server.close()
+    
 
 
 
@@ -88,4 +101,104 @@ def take_user_input():
     return query
 
 
-take_user_input()
+# take_user_input()
+
+
+
+
+paths = {
+    'notepad': "C:\\Windows\\notepad.exe",
+    
+    'calculator': "C:\\Windows\\System32\\calc.exe"
+}
+
+def open_notepad():
+    os.startfile(paths['notepad'])
+
+def open_cmd():
+    os.system('start cmd')
+  
+def open_camera():
+    sp.run('start microsoft.windows.camera:', shell=True)  
+    
+def open_calculator():
+    sp.Popen(paths['calculator'])
+    
+def find_my_ip():
+    ip_address = requests.get('https://api64.ipify.org?format=json').json()
+    return ip_address["ip"]
+
+def search_on_wikipedia(query):
+    results = wikipedia.summary(query, sentences=2)
+    return results
+def search_on_google(query):
+    kit.search(query)
+
+def  weather_forcast(city):
+    res = requests.get(
+        f"http://api.openweathermap.org/data/2.5/weather?q="+city+"&appid=11112d61dad6beb077f4c80f5b7b4156&units=metric").json()
+    weather = res["weather"][0]["main"]
+    temperature = res["main"]["temp"]
+    feels_like = res["main"]["feels_like"]
+    return weather, f"{temperature}℃", f"{feels_like}℃"
+
+    
+if __name__ == '__main__':
+   
+    greet_user()
+    while True:
+        query = take_user_input().lower()
+
+        if 'open notepad' in query:
+            open_notepad()
+            
+        elif 'open command prompt' in query or 'open cmd' in query:
+            open_cmd()
+        elif 'open camera' in query:
+            open_camera()    
+          
+        elif 'open calculator' in query:
+            open_calculator()    
+          
+        elif 'exit' in query or 'quit' in query:
+           exit()
+        elif 'wikipedia' in query:
+            speak('What do you want to search on Wikipedia, sir?')
+            search_query = take_user_input().lower()
+            results = search_on_wikipedia(search_query)
+            speak(f"According to Wikipedia, {results}")
+            speak("For your convenience, I am printing it on the screen sir.")
+            print(results)
+        
+        elif 'youtube' in query:
+            search_on_google("youtube")
+            exit()
+        elif 'search on google' in query or 'search' in query:
+            speak('What do you want to search on Google, sir?')
+            query = take_user_input().lower()
+            search_on_google(query)
+            exit()
+        elif 'send email' in query or 'email' in query:
+                try:
+                    speak("whome do you want to send email")
+                    to = take_user_input().lower()
+                    
+                    speak("What should I say?")
+                    content = take_user_input().lower()
+                     
+                    sendEmail(to, content)
+                    speak("Email has been sent!")
+                except Exception as e:
+                   
+                    speak("Sorry"+USERNAME+ "I am not able to send this email")
+        elif 'weather' in query or 'weather report' in query:
+            ip_address = find_my_ip()
+            city = requests.get(f"https://ipapi.co/{ip_address}/city/").text
+            speak(f"Getting weather report for your city {city}")
+            weather, temperature, feels_like =  weather_forcast(city)
+            speak(f"The current temperature is {temperature}, but it feels like {feels_like}")
+            speak(f"Also, the weather report talks about {weather}")
+            speak("For your convenience, I am printing it on the screen sir.")
+            print(f"Description: {weather}\nTemperature: {temperature}\nFeels like: {feels_like}")
+            
+            
