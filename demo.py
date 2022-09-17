2545b1fa98afae2021c96aa220beca11cbce79d4
Ayusht777 <talesarayush@gmail.com>
2022-09-17 17:24:16 +0530

    working-stage-1

--- demo.py
+++ demo.py
@@ -1,21 +1,91 @@
-import pyttsx3
-from decouple import config
+import pyttsx3 # for text to speech
+from datetime import datetime # for time
+import speech_recognition as user_audio # user input by voice
+import pyaudio
 
-#USERNAME = config('USER')
-#BOTNAME = config('BOTNAME')
 
 
-engine = pyttsx3.init('sapi5')
 
-# Set Rate
-engine.setProperty('rate', 190)
 
-# Set Volume
-engine.setProperty('volume', 1.0)
+USERNAME = 'AYUSH'
+BOTNAME = 'ALEXA'
 
-# Set Voice (Female)
-voices = engine.getProperty('voices')
-print(voices[0].id)
-for i in voices:
-    print(i.id)
-engine.setProperty('voice', voices[1].id)
+
+# main engine
+engine = pyttsx3.init()
+
+""" RATE"""
+rate = engine.getProperty('rate')   # getting details of current speaking rate
+# print (rate)                        #printing current voice rate
+engine.setProperty('rate', 110)     # setting up new voice rate
+
+
+"""VOICE"""
+voices = engine.getProperty('voices')       #getting details of current voice
+#engine.setProperty('voice', voices[0].id)  #changing index, changes voices. o for male
+engine.setProperty('voice', voices[1].id)   #changing index, changes voices. 1 for female
+
+# for i in voices:
+#     print(i.id)
+
+"""VOLUME"""
+volume = engine.getProperty('volume')   #getting to know current volume level (min=0 and max=1)
+# print (volume)                          #printing current volume level
+engine.setProperty('volume',1.0)    # setting up volume level  between 0 and 1
+
+
+
+# Text to Speech Conversion
+def speak(text):
+    """Used to speak whatever text is passed to it"""
+    
+    engine.say(text)
+    engine.runAndWait()
+    
+
+def greet_user():
+    """Greets the user according to the time"""
+    
+    hour = datetime.now().hour
+    if (hour >= 6) and (hour < 12):
+        speak(f"Good Morning {USERNAME}")
+    elif (hour >= 12) and (hour < 16):
+        speak(f"Good afternoon {USERNAME}")
+    elif (hour >= 16) and (hour < 19):
+        speak(f"Good Evening {USERNAME}")
+    speak(f"I am {BOTNAME}. How may I assist you?")
+    
+
+# greet_user()
+
+
+
+
+def take_user_input():
+    """Takes user input, recognizes it using Speech Recognition module and converts it into text"""
+
+    r = user_audio.Recognizer()
+    with user_audio.Microphone() as source:
+        print('Listening....')
+        r.pause_threshold = 1 # limit of audio # time for audio capturing
+        audio = r.listen(source)
+
+    try:
+        print('Recognizing...')
+        query = r.recognize_google(audio, language='en-in')
+        if not 'exit' in query or 'stop' in query:
+            speak(query)
+        else:
+            hour = datetime.now().hour
+            if hour >= 21 and hour < 6:
+                speak("Good night sir, take care!")
+            else:
+                speak('Have a good day sir!')
+            exit()
+    except Exception:
+        speak('Sorry, I could not understand. Could you please say that again?')
+        query = 'None'
+    return query
+
+
+take_user_input()
